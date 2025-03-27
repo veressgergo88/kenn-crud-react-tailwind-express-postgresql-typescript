0a925ac600ac81de0769485cd74b0c9ff3297335
veressgergo88 <veress.gergo88@gmail.com>
2025-03-27 08:14:50 +0100

    initial; Vite, Tailwind, DaisyUI installed; Navbar component created

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center">
+        
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
